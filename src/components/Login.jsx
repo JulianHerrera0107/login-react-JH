91c3dac98949079f7496749fda36a3c51dee872f
Julian-Herrera <juliandavih@gmail.com>
2024-12-19 13:21:10 -0500

    Agregando el useState para almacenar info de User y Password acompañado de Error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react"
+
 
 export function Login(){
-
+    //State para capturar información
+    const [user, setUser] = useState("")
+    const [password, setPassword] = useState("")
+    //State para capturar el error
+    const [error, setError] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault() //Previene refrescar la página
+        if(user === "" || password === ""){
+            setError(true)
+            return
+        }
+    }
 
     return (
         <>
-        <div className="wrapper">
+        <div className="wrapper"
+        onSubmit={handleSubmit}>
         <div className="container main">
             <div className="row">
                 <div className="col-md-6 side-image">
@@ -18,18 +32,22 @@ export function Login(){
                     <div className="input-box">
                         <header>Iniciar Sesion</header>
                         <div className="input-field">
-                            <input type="text" className="input" id="correo" required autoComplete="off"></input>
+                            <input type="text" className="input" 
+                            id="correo" required autoComplete="off"
+                            value={user} onChange={e => setUser(e.target.value)} />
                             <label htmlFor="correo">Correo Electronico</label>
                         </div>
                         <div className="input-field">
-                            <input type="password" className="input" id="contraseña" required autoComplete="off"></input>
+                            <input type="password" className="input" id="contraseña" required autoComplete="off" 
+                            value={password} onChange={e => setPassword(e.target.value)} />
                             <label htmlFor="contraseña">Contraseña</label>
                         </div>
                         <div className="input-field">
-                            <input type="submit" className="submit" value="Ingresar"></input>
+                            <input type="submit" className="submit" value="Ingresar" />
                         </div>
                         <div className="signin">
                             <span>¿No tienes una cuenta? <a href="a">Registrate aquí</a></span>
+                            {error && <span>Todos los campos son obligatorios</span>}
                         </div>
                     </div>
                 </div>
