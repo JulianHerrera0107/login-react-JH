5bc355f41772af5f19a0453622e49df4b09d31b4
Julian-Herrera <juliandavih@gmail.com>
2024-12-19 12:23:04 -0500

    Renombrando las etiquetas de HTML por JS (className, htmlFor y autoComplete)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,34 +2,33 @@
 export function Login(){
 
 
-    
     return (
         <>
-        <div class="wrapper">
-        <div class="container main">
-            <div class="row">
-                <div class="col-md-6 side-image">
+        <div className="wrapper">
+        <div className="container main">
+            <div className="row">
+                <div className="col-md-6 side-image">
                     {/*<!---Logo Escappy--->*/}
                     <img src="/img/logo.png" alt="Logo Escappy Travel"></img>
-                    <div class="text">
+                    <div className="text">
                         <p>Viajes sorpresa y personalizados según tus deseos. <i>Escappy Travel</i></p>
                     </div>
                 </div>
-                <div class="col-md-6 right">
-                    <div class="input-box">
+                <div className="col-md-6 right">
+                    <div className="input-box">
                         <header>Iniciar Sesion</header>
-                        <div class="input-field">
-                            <input type="text" class="input" id="correo" required autocomplete="off"></input>
-                            <label for="correo">Correo Electronico</label>
+                        <div className="input-field">
+                            <input type="text" className="input" id="correo" required autoComplete="off"></input>
+                            <label htmlFor="correo">Correo Electronico</label>
                         </div>
-                        <div class="input-field">
-                            <input type="password" class="input" id="contraseña" required autocomplete="off"></input>
-                            <label for="contraseña">Contraseña</label>
+                        <div className="input-field">
+                            <input type="password" className="input" id="contraseña" required autoComplete="off"></input>
+                            <label htmlFor="contraseña">Contraseña</label>
                         </div>
-                        <div class="input-field">
-                            <input type="submit" class="submit" value="Ingresar"></input>
+                        <div className="input-field">
+                            <input type="submit" className="submit" value="Ingresar"></input>
                         </div>
-                        <div class="signin">
+                        <div className="signin">
                             <span>¿No tienes una cuenta? <a href="a">Registrate aquí</a></span>
                         </div>
                     </div>
