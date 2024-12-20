eac95bd29f7f600c672059d786aeb9ca4c7be9f0
Julian-Herrera <juliandavih@gmail.com>
2024-12-20 10:44:42 -0500

    Optimizando la interfaz de Home.jsx junto a los estilos en CSS

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,24 @@ export function Home({ usuario, setUsuario }) {
     }
     return (
         <>
-            <div className="inicio">
+            {/*<div className="inicio">
                 <h1>Bienvenido {usuario}</h1>
-                {/*<img src="/img/costa-caribe.jpg"></img>*/}
+                <img src="/img/costa-caribe.jpg"></img>
                 <button onClick={handleLogout}>Cerrar Sesion</button>
+            </div>*/}
+            <div class="container-nav">
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Productos Escappy</a></li>
+                        <li><a href="#">Contactanos</a></li>
+                        <li><a href="#" onClick={handleLogout}>Cerrar Sesion</a></li>
+                    </ul>
+                </nav>
             </div>
+            <section class="container-body">
+                <h1>Bienvenido al Sistema - {usuario}</h1>
+            </section>
         </>
     )
 }
